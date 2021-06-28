ff289fcc6210a35a1105317764c4f4f239dbfbc7
rds80 <rippleshah@msn.com>
2021-06-28 19:53:08 -0400

    using Django test client

--- virtualenv/Scripts/lists/tests.py
+++ virtualenv/Scripts/lists/tests.py
@@ -1,8 +1,11 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
